3b5184312e8536f70d4b239e2afe802f2cf6cc13
Nathan Duckett <nduckett13@gmail.com>
2020-04-27 22:37:39 +1200

    Add alert user on action | Verify incoming input is valid (WIP)

--- app/src/main/java/page/ndser/remotewakeup/MainActivity.java
+++ app/src/main/java/page/ndser/remotewakeup/MainActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.Arrays;
@@ -43,6 +46,22 @@ public class MainActivity extends AppCompatActivity {
         String mac = getTextContents(R.id.macInput);
         String port = getTextContents(R.id.portInput);
 
+
+        // Verify contents first.
+        if (!verifyHostname(hostname)) {
+            alert(view, R.string.host_fail_message);
+            return;
+        }
+        if (!verifyMac(mac)) {
+            alert(view, R.string.mac_fail_message);
+            return;
+        }
+        if (!verifyPort(port)) {
+            alert(view, R.string.port_fail_message);
+            return;
+        }
+
+
         // Create object with values
         WakeupHistory details = new WakeupHistory(mac, hostname, port);
 
@@ -55,10 +74,66 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }).start();
+
+        Snackbar notification = Snackbar.make(view, R.string.sent_message, Snackbar.LENGTH_SHORT);
+        notification.show();
     }
 
-    // Get the text from the text edit view.
+    /**
+     * Get the text from the text edit view.
+     *
+     * @param id Id of the EditText box to retrieve the contents from.
+     * @return String which was contained inside the text box.
+      */
     private String getTextContents(int id) {
         return ((EditText) findViewById(id)).getText().toString();
+    }
+
+    /**
+     * Display a snackbar alert for the user.
+     *
+     * @param thisView The current view from the application.
+     * @param string_id ID code of the string to be displayed in the snack bar.
+     */
+    private void alert(View thisView, int string_id) {
+        Snackbar.make(thisView, string_id, Snackbar.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Verify that the hostname exists and is valid.
+     *
+     * @param hostname Hostname expected to send the packet to.
+     * @return Boolean indicating if this hostname exists.
+     */
+    private boolean verifyHostname(String hostname) {
+        return !hostname.equals("");
+    }
+
+    /**
+     * Verify that the MAC address is valid.
+     *
+     * @param mac String MAC address extracted from the application.
+     * @return Boolean indicating if this MAC address is valid.
+     */
+    private boolean verifyMac(String mac) {
+        String[] splitMac = mac.split(":");
+
+        return splitMac.length == 6;
+    }
+
+    /**
+     * Verify that the port number provided is both a number and within the speicified port range
+     * 0 -> 65535 for valid ports.
+     *
+     * @param port String port number extracted from the Android view.
+     * @return Boolean indicating if the port is valid.
+     */
+    private boolean verifyPort(String port) {
+        try {
+            int portNumber = Integer.parseInt(port);
+            return portNumber >= 0 && portNumber < 65535;
+        } catch(NumberFormatException e) {
+            return false;
+        }
     }
 }
